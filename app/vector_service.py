a32ba95c68d9220688d08b6d525417cae41fbf64
aman.b <aman.bhandari@grazitti.com>
2025-09-28 15:34:39 +0530

    caption model upgrade

--- app/vector_service.py
+++ app/vector_service.py
@@ -7,8 +7,9 @@ def describe_image(uploaded_file):
     print('up------',uploaded_file)
 
     # Generate caption using BLIP
-    inputs = processor(images=uploaded_file,return_tensors="pt")
-    out = model.generate(**inputs,num_beams=5,
-    max_length=50,early_stopping=True)
+    prompt = "Describe this image vividly, including objects, colors, and context."
+    inputs = processor(images=uploaded_file, text=prompt ,return_tensors="pt")
+    out = model.generate(**inputs,num_beams=7,
+    max_length=100,early_stopping=True)
     caption = processor.decode(out[0], skip_special_tokens=True)
     return caption
--- models/blip.py
+++ models/blip.py
@@ -1,4 +1,4 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
